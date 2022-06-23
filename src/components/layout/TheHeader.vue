370b14600a0ae8ed3c4c3867db395993080e5532
Niels <niels_martens@live.nl>
2022-06-23 16:17:40 +0200

    init commit

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+
+
+export default {
+    props: ['title']
+}
+</script>
+
+<style defer>
+
+header {
+    background-color: rgb(77, 120, 77);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+}
+
+header h1 {
+    color: white;
+}
+
+</style>
